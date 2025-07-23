558953d1fbb509c5766d6c424f24cb6349a759ac
mernspace <sifat355y@.com>
2025-07-23 12:00:18 +0600

    Full page content updated

--- src/app/documents/[documentid]/page.tsx
+++ src/app/documents/[documentid]/page.tsx
@@ -1,30 +1,32 @@
 "use server";
+
 import { auth } from "@clerk/nextjs/server";
 import { Id } from "../../../../convex/_generated/dataModel";
-import { DocumentPage } from './document'
-import { preloadQuery } from 'convex/nextjs'
+import { DocumentPage } from "./document";
+import { preloadQuery } from "convex/nextjs";
 import { api } from "../../../../convex/_generated/api";
 
 interface DocumentsIdPageProps {
-    params: { documentId: Id<"Documents"> }
+    params: { documentId: string };
 }
+
 const DocumentIdPage = async ({ params }: DocumentsIdPageProps) => {
-    const { documentId } = await params;
-    const { getToken } = await auth()
-    const token = await getToken({ template: "convex" }) ?? undefined
+    const documentId = params.documentId as Id<"Documents">;
+
+    const { getToken } = await auth();
+    const token = (await getToken({ template: "convex" })) ?? undefined;
 
     if (!token) {
-        throw new Error("Unauthorized")
+        throw new Error("Unauthorized");
     }
+
     const preloadedDocument = await preloadQuery(
         api.document.getById,
         { id: documentId },
         { token }
-    )
+    );
 
+    return <DocumentPage preloadedDocument={preloadedDocument} />;
+};
 
-    return <DocumentPage preloadedDocument={preloadedDocument} />
-}
-
-
-export default DocumentIdPage
+export default DocumentIdPage;
