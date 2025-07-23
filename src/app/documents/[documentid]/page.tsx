a68af649b3f18552037a153c133b0c07dc3ba17f
mernspace <sifat355y@.com>
2025-07-23 11:48:01 +0600

    document poge props updated

--- src/app/documents/[documentid]/page.tsx
+++ src/app/documents/[documentid]/page.tsx
@@ -6,11 +6,10 @@ import { preloadQuery } from 'convex/nextjs'
 import { api } from "../../../../convex/_generated/api";
 
 interface DocumentsIdPageProps {
-    params: Promise<{ documentId: Id<"Documents"> }>
+    params: { documentId: Id<"Documents"> }
 }
-
 const DocumentIdPage = async ({ params }: DocumentsIdPageProps) => {
-    const { documentId } = await params;
+    const { documentId } = params;
     const { getToken } = await auth()
     const token = await getToken({ template: "convex" }) ?? undefined
 
